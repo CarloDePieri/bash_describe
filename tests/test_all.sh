b1f10e03cc31fa34cd2a7192e822f5c067af4f1e
Carlo De Pieri <depieri.carlo@gmail.com>
2021-09-12 15:23:38 +0200

    move test cases; add helpers functions

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,20 +1,29 @@
+# constants
+test_cases="test_cases"
+collection="$test_cases/tests_collection"
+format="$test_cases/tests_format"
 
-echo
+# helpers
+bd() {
+  # Convenient way to run bash_describe against one or multiple target with the run helper function
+  targets="$@"
+  cmd() { ../bash_describe $targets; }
+  run cmd
+}
 
 # tests collection
+
 # TODO
 it "should be able to collect tests from a single file"
   assert() {
-    cmd() { ../bash_describe tests_collection/test_success.sh; }
-    run cmd
+    bd $collection/test_success.sh
     exit_code_is 0 && output_contains "it should work well" && output_contains "it should work too"
   }
 ti
 
 it "should be able to collect tests from multiple files"
   assert() {
-    cmd() { ../bash_describe tests_collection/test_success.sh tests_collection/test_fail.sh; }
-    run cmd
+    bd $collection/test_success.sh $collection/test_fail.sh
     output_contains "it should work well" && \
       output_contains "it should work too" && \
       output_contains "it should fail"
@@ -24,8 +33,7 @@ ti
 
 it "should be able to collect tests from a folder"
   assert() {
-    cmd() { ../bash_describe tests_collection; }
-    run cmd
+    bd $collection
     output_contains "it should work well" && \
       output_contains "it should work too" && \
       output_contains "it should fail"
@@ -35,8 +43,7 @@ ti
 
 it "should skip files with wrong format from a folder"
   assert() {
-    cmd() { ../bash_describe tests_collection; }
-    run cmd
+    bd $collection
     output_contains "it should work well" && \
       output_contains "it should work too" && \
       output_contains "it should fail" && \
@@ -47,8 +54,7 @@ ti
 
 it "should run tests from a wrong name format file if expressly told to"
   assert() {
-    cmd() { ../bash_describe tests_collection/t_skip.sh; }
-    run cmd
+    bd $collection/t_skip.sh
     output_contains "it should skip" && \
       exit_code_is 0
   }
@@ -56,8 +62,7 @@ ti
 
 it "should complain if a wrong path is given as argument"
   assert() {
-    cmd() { ../bash_describe tests_collection/nothere; }
-    run cmd
+    bd $collection/notthere
     exit_code_is 1 && output_contains "is not a valid folder or file"
   }
 ti
@@ -65,8 +70,7 @@ ti
 # TODO
 # it "should complain if no test is collected"
   # assert() {
-    # cmd() { ../bash_describe tests_collection/empty; }
-    # run cmd
+    # bd $collection/empty
     # exit_code_is 1
   # }
 # ti
@@ -74,51 +78,45 @@ ti
 echo
 
 # tests format
-# TODO make poorly formatted test SKIP, not die
+# TODO make poorly formatted test SKIP, not die ?
 it "should die with a missing it"
   assert() {
-    cmd() { ../bash_describe test_format/missing_it; }
-    run cmd
+    bd $format/missing_it
     exit_code_is 1 && output_contains "ERR: \`ti\` called without a matching \`it\`"
   }
 ti
 
 it "should die with a missing ti"
   assert() {
-    cmd() { ../bash_describe test_format/missing_ti; }
-    run cmd
+    bd $format/missing_ti
     exit_code_is 1 && output_contains "ERR: \`it\` called, but a previous \`ti\` seems missing"
   }
 ti
 
 it "should die with a missing assert function"
   assert() {
-    cmd() { ../bash_describe test_format/missing_assert; }
-    run cmd
+    bd $format/missing_assert
     exit_code_is 1 && output_contains "ERR: no assert function was found. Define one"
   }
 ti
 
 it "should die with an assert function defined outside it/ti blocks"
   assert() {
-    cmd() { ../bash_describe test_format/defined_assert; }
-    run cmd
+    bd $format/defined_assert
     exit_code_is 1 && output_contains "a function called assert was already defined:"
   }
 ti
 
 it "should take the name from it"
   assert() {
-    cmd() { ../bash_describe test_format/given_name; }
-    run cmd
+    bd $format/given_name
     exit_code_is 0 && output_contains "it has a name"
   }
 ti
 
 it "should have a default name"
   assert() {
-    cmd() { ../bash_describe test_format/no_name; }
-    run cmd
+    bd $format/no_name
     exit_code_is 0 && output_contains "it should work"
   }
 ti
